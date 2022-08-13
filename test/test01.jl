443718592e5581bec48c11f778b49733f8ce7762
Nick Mayhall <nmayhall@vt.edu>
2022-08-13 09:27:49 -0400

    added multiroot tests

--- test/test01.jl
+++ test/test01.jl
@@ -25,4 +25,17 @@ using LinearMaps
     e_ref = -18.260037795157675
     @test isapprox(e[1], -18.260037795157675)
 
+    e_ref = [
+             -18.260037795157675
+             -17.9716411644818
+             -17.47598854674256
+             -17.184105784827796
+             -16.939563610543257
+             -16.83937885452674
+            ]
+    # now with more settings specified and roots
+    dav = Davidson(lmap; max_iter=200, max_ss_vecs=8, tol=1e-6, nroots=6, v0=nothing, lindep_thresh=1e-10)
+    e,v = solve(dav)
+    @test all(isapprox.(e, e_ref, atol=1e-10))
+
 end
